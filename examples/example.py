7cb4d97cf6efdbb7ce44183898ea26946ae1d686
sherbold <sherbold@users.noreply.github.com>
2020-01-14 15:50:44 +0000

    Automated deployment: Tue Jan 14 15:50:44 UTC 2020 8394db89f6811832c56aa3eb6697113f9bdebb4d

---
+++ examples/example.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from autorank.autorank import autorank, create_report, plot_stats, latex_table
+
+np.random.seed(42)
+pd.set_option('display.max_columns', 7)
+std = 0.3
+means = [0.2, 0.3, 0.5, 0.8, 0.85, 0.9]
+sample_size = 50
+data = pd.DataFrame()
+for i, mean in enumerate(means):
+    data['pop_%i' % i] = np.random.normal(mean, std, sample_size).clip(0, 1)
+
+res = autorank(data, alpha=0.05, verbose=False)
+print(res)
+create_report(res)
+plot_stats(res)
+plt.show()
+latex_table(res)
+
