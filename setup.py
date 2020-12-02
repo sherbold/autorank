d05bffb92a778523972b361c6665a92cf9ec2086
sherbold <herbold@cs.uni-goettingen.de>
2020-12-02 11:14:49 +0100

    Prepare for release 1.1.0

--- setup.py
+++ setup.py
@@ -9,8 +9,9 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='autorank',
-    version='1.1.0.dev',
-    description='Automated ranking of populations in a repeated measures experiment, e.g., to rank different machine learning approaches tested on the same data.',
+    version='1.1.0',
+    description='Automated ranking of populations in a repeated measures experiment, e.g., to rank different machine '
+                'learning approaches tested on the same data.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=['numpy', 'pandas>=0.25.0', 'statsmodels>=0.10.0', 'scipy>=1.3.0', 'matplotlib>=3.1.3',
